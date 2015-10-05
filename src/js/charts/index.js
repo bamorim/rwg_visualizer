5b236e0cacf80840a9d9aa2a60bc205ead2ced95
Bernardo Amorim <contato@bamorim.com>
2015-10-05 00:59:27 -0300

    Add HOC to add grid on all ticks

--- src/js/charts/index.js
+++ src/js/charts/index.js
@@ -5,10 +5,19 @@ let Log10Grid = (SuperChart) => class Log10Grid extends SuperChart {
   _gridTicks(){
     let { xScale, yScale } = this._scales();
     let only10power = function(x) { return Math.log10(x) % 1 == 0 };
-    var xGridTicks = xScale.ticks().filter(only10power)
-    var yGridTicks = yScale.ticks().filter(only10power)
+    var xGridTicks = xScale.ticks().filter(only10power);
+    var yGridTicks = yScale.ticks().filter(only10power);
     return {xGridTicks, yGridTicks};
   }
 }
 
-export default { Chart, Log10Grid, ScatterChart };
+let TickGrid = (SuperChart) => class TickGrid extends SuperChart {
+  _gridTicks(){
+    let { xScale, yScale } = this._scales();
+    var xGridTicks = xScale.ticks();
+    var yGridTicks = yScale.ticks();
+    return {xGridTicks, yGridTicks};
+  }
+}
+
+export default { Chart, Log10Grid, ScatterChart, TickGrid };
